b99d1ae831ea6d584d1a6259b689c64fcbec0133
CodeEspritLibre <espritlibre@t-online.de>
2020-04-13 19:12:32 +0200

    Fixed README license

--- songe-lib/crc16.rb
+++ songe-lib/crc16.rb
@@ -1,7 +1,27 @@
 # Part of digest-crc gem project
 # Source: https://github.com/postmodern/digest-crc/blob/master/lib/digest/crc.rb
 #         https://github.com/postmodern/digest-crc/blob/master/lib/digest/crc16.rb
-# under MIT license: https://github.com/postmodern/digest-crc/blob/master/LICENSE.txt
+#
+# Copyright (c) 2010-2020 Hal Brodigan
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# 'Software'), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 module Digest
   #
